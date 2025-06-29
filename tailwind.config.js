927eb4e4b3bee5cfa96d8f74530cff3616d837e1
Karan <karansharma192004@gmail.com>
2025-06-29 16:58:53 +0530

    Adding v0

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        jersey: ['"Jersey 10"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
