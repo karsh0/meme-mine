30127312ba1397ef3793c8b1bba6b8253955cf87
Karan <karansharma192004@gmail.com>
2025-08-02 17:04:48 +0530

    Adding more memes

--- data/templates.ts
+++ data/templates.ts
@@ -11,10 +11,70 @@ export const templates = {
     "butterfly":{
         image:"./temp4.jpg"
     },
+    "cheating-coldplay":{
+        image:"./temp26.jpg"
+    },
     "kiddy":{
         image:"./temp5.jpg"
     },
     "ballons":{
         image:"./temp6.jpg"
     },
+    "sign-board":{
+        image:"./temp7.jpg"
+    },
+    "trump-bro":{
+        image:"./temp8.jpg"
+    },
+    "cello-tape":{
+        image:"./temp9.jpg"
+    },
+    "spider-man":{
+        image:"./temp10.jpg"
+    },
+    "train-rush":{
+        image:"./temp11.jpg"
+    },
+    "dragon-heads":{
+        image:"./temp12.jpg"
+    },
+    "bus-ride":{
+        image:"./temp13.jpg"
+    },
+    "selfie":{
+        image:"./temp14.jpg"
+    },
+    "ghost-time":{
+        image:"./temp15.jpg"
+    },
+    "corporate":{
+        image:"./temp16.jpg"
+    },
+    "obama":{
+        image:"./temp17.jpg"
+    },
+    "employee":{
+        image:"./temp18.jpg"
+    },
+    "froggy":{
+        image:"./temp19.jpg"
+    },
+    "bean":{
+        image:"./temp20.jpg"
+    },
+    "oh-yeah":{
+        image:"./temp21.jpg"
+    },
+    "math-lady":{
+        image:"./temp22.jpg"
+    },
+    "girl-on-couch":{
+        image:"./temp23.jpg"
+    },
+    "horse hard":{
+        image:"./temp24.jpg"
+    },
+    "cute cat":{
+        image:"./temp25.jpg"
+    }
 }
